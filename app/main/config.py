4ad4b9fa741697a1ce99eb56958ca2ebd56a4f13
Alamin AI <alamin.ibrahimfip@flexisaf.net>
2025-03-08 18:14:27 +0100

    added url formatting

--- app/main/config.py
+++ app/main/config.py
@@ -14,4 +14,4 @@ ENVIRONMENT = 'Sandbox'
 # Set to latest at the time of updating this app, can be be configured to any minor version
 API_MINORVERSION = '75'
 
-GOOGLE_AI_API_KEY=""
+GOOGLE_AI_API_KEY="AIzaSyBDV-cXr2SooYhz_lIYWrnnP44nCH5_1lU"
